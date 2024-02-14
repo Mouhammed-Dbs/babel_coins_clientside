74bdefee9c2a12c0a7630b71053e6a84b9ebd222
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-02-14 12:01:25 +0200

    fix card balance page

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -36,17 +36,26 @@ export default function Balance() {
   }, []);
   return (
     // <MyCoins>
-    <div className="m-auto">
+    <div className="container m-auto">
+      <div className="w-full md:w-[520px] lg:w-[790px] m-auto my-4">
+        <h1 className="w-fit text-xl font-bold border-b-1 border-black dark:border-white">
+          BALANCE
+        </h1>
+      </div>
+
       <div className="w-full text-center">
         <h1 className="w-fit m-auto my-4 text-xl font-bold border-b-1 border-black dark:border-white">
           All Accounts
         </h1>
-        <div className="w-fit no-scrollbar m-auto mb-14 pt-7 pb-36 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center overflow-y-scroll h-screen">
+        <div className="w-fit h-screen no-scrollbar m-auto pt-7 pb-[180px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center overflow-y-scroll">
           {coins.map((coin) => (
             <div
               key={coin.currencyName}
-              className="relative w-64 h-min mt-3 mr-2 p-4 bg-neutral-100 dark:bg-default-100 text-center rounded-md shadow-sm"
+              className="relative w-64 h-min mt-2 mr-2 p-4 bg-neutral-100 dark:bg-default-100 text-center rounded-md shadow-sm"
             >
+              <div className="absolute overflow-hidden bg-default-300 dark:bg-default-50 rounded-full w-16 h-16 top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="absolute w-full h-1/2 top-1/2 bg-neutral-100 dark:bg-default-100"></div>
+              </div>
               <div className="absolute bg-neutral-100 dark:bg-default-100 rounded-full w-12 h-12 top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <Image
                   className="p-1"
@@ -56,8 +65,8 @@ export default function Balance() {
                   alt=""
                 ></Image>
               </div>
-              <div className="absolute bg-default-300 dark:bg-default-50 rounded-full w-14 h-14 bottom-0 left-1/2 transform -translate-x-1/2 translate-y-2/3"></div>
-              <div className="w-full mt-2">
+
+              <div className="w-full mt-3">
                 <h1 className="text-lg">{coin.currencyName}</h1>
                 <h1 className="text-xs">{coin.network}</h1>
                 <h1 className="font-bold text-lg">{coin.balance}</h1>
