8e537a321d8b3e465c221abf219248f23f9459e7
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-02-27 11:56:29 +0200

    edit color in MyInput for global using

--- src/components/utils/MyInput.js
+++ src/components/utils/MyInput.js
@@ -10,9 +10,11 @@ export default function MyInput({
   readOnly = false,
   textColor,
   color,
-  selectedBorderColor,
-  selectedLabelColor,
+  selectedBorderColor = "border-cyan-300",
+  selectedLabelColor = "text-cyan-300",
+  linkColor = "text-cyan-300",
 }) {
+  //border-cyan-300
   const { name, type, placeholder, label } = item;
   return (
     <div className={`relative ${className}`}>
@@ -20,9 +22,9 @@ export default function MyInput({
         name={name}
         onChange={handleChange}
         value={value}
-        className={`peer w-full self-center placeholder-slate-300 mt-6 rounded-lg border-2 text-xs p-2 bg-inherit focus:outline-none focus:border-cyan-300 ${
+        className={`peer w-full self-center placeholder-slate-300 mt-6 rounded-lg border-2 text-xs p-2 bg-inherit focus:outline-none focus:${selectedBorderColor} ${
           value
-            ? "border-cyan-300"
+            ? selectedBorderColor
             : color != undefined
             ? color
             : "border-white border-opacity-35"
@@ -35,7 +37,7 @@ export default function MyInput({
       {withLink !== undefined ? (
         <Link
           href={withLink.href}
-          className="text-cyan-300 text-xs absolute -bottom-0 -right-0 mb-3 mr-2"
+          className={`${linkColor} text-xs absolute -bottom-0 -right-0 mb-3 mr-2`}
         >
           {withLink.nameLink}
         </Link>
@@ -43,8 +45,8 @@ export default function MyInput({
         ""
       )}
       <label
-        className={`absolute -top-0 -left-0 text-sm ml-1 mb-1 peer-focus:text-cyan-300 text-opacity-80 ${
-          value ? "text-cyan-300" : color != undefined ? color : "text-white"
+        className={`absolute -top-0 -left-0 text-sm ml-1 mb-1 peer-focus:${selectedLabelColor} text-opacity-80 ${
+          value ? selectedLabelColor : color != undefined ? color : "text-white"
         }`}
       >
         {label}
