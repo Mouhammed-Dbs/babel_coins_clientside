5a3965cc04a194d818dadbb73501a0c5b07b4d9a
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-03-04 18:50:07 +0200

    continue

--- src/pages/account/send.js
+++ src/pages/account/send.js
@@ -6,61 +6,28 @@ import { IoIosArrowDown } from "react-icons/io";
 import axios from "axios";
 import MyInput from "@/components/utils/MyInput";
 
-export default function Add(props) {
+export default function Send(props) {
   const router = useRouter();
+  const [templates, setTemplates] = useState(["b23523553", "b29523553"]);
   const [accounts, setAccounts] = useState([
     { type: "crypto", name: "BTC", balance: "200" },
     { type: "crypto", name: "ETH", balance: "50" },
   ]);
   const [fiatAccounts, setFiateAccounts] = useState(["USD", "EUR", "RUB"]);
 
-  const getCoins = async () => {
-    try {
-      // setLoading(true);
-      const res = await axios.get(
-        `${process.env.BASE_API_URL}/users/all-balances`,
-        {
-          headers: {
-            Authorization: localStorage.getItem("babel-coins-user-token"),
-          },
-        }
-      );
-      const result = res.data;
-      if (!result.error) {
-        coinsArray = Array();
-        result.data.map((coin) => {
-          coinsArray.push({
-            type: "crypto",
-            name: coin.network,
-            balance: coin.balance,
-          });
-        });
-        console.log(result);
-        setAccounts(coinsArray);
-      } else {
-        // setLoading(false);
-      }
-    } catch (error) {
-      // setLoading(false);
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    // getCoins();
-  }, []);
   return (
     <div className="h-screen container m-auto no-scrollbar overflow-y-scroll pb-[150px]">
       <div className="w-full md:w-[520px] lg:w-[790px] m-auto mt-4 pb-3">
         <div className="w-fit ml-4 md:ml-0 bg-gradient-to-r from-black dark:from-slate-300 via-gray-600 to-default-300 dark:bg-default-50 pb-[2px]">
           <h1 className="w-fit text-lg md:text-2xl font-bold bg-slate-50 dark:bg-default-50">
-            ADD
+            TRANSFER
           </h1>
         </div>
       </div>
-      <div className="p-4 py-10 md:px-8 ml-4 mt-6 md:m-auto md:mt-10 w-11/12 md:w-[520px] lg:w-[790px] md:text-center backdrop-blur-xs bg-white dark:bg-default-100 rounded-lg shadow-md">
-        <div className="md:flex w-fit md:w-full m-auto gap-4 items-center">
+      <div className="p-4 py-10 md:px-8 ml-4 mt-6 md:m-auto md:mt-10 w-11/12 md:w-[520px] lg:w-[790px] text-center backdrop-blur-xs bg-white dark:bg-default-100 rounded-lg shadow-md">
+        <div className="flex w-full gap-4 items-center">
           <label className="text-right text-sm md:text-base w-36">
-            Choose account
+            Choose system
           </label>
           <Select
             aria-label="none"
@@ -68,10 +35,10 @@ export default function Add(props) {
             size="sm"
             items={accounts}
             labelPlacement="outside"
-            placeholder="CHOOSE ACCOUNT"
+            placeholder="CHOOSE SYSTEM"
             selectorIcon={<IoIosArrowDown color="var(--bg-primary-color)" />}
             classNames={{
-              base: "p-[2px] max-w-xs peer w-48 md:w-64 self-center rounded-lg border-2 dark:border-slate-400 border-black border-opacity-55 text-xs bg-inherit focus:outline-none focus:border-cyan-300",
+              base: "p-[2px] max-w-xs peer w-52 md:w-64 self-center rounded-lg border-2 dark:border-slate-400 border-black border-opacity-55 text-xs bg-inherit focus:outline-none focus:border-cyan-300",
               trigger: "h-6",
             }}
             renderValue={(items) => {
@@ -113,63 +80,31 @@ export default function Add(props) {
             )}
           </Select>
         </div>
-        <div className="md:flex w-fit md:w-full m-auto gap-4 items-center mt-3">
+        <div className="flex w-full gap-4 items-center mt-3">
           <label className="text-right text-sm md:text-base w-36">
-            Choose system
+            Templates
           </label>
           <Select
             aria-label="none"
             style={{ backgroundColor: "inherit" }}
             size="sm"
-            items={accounts}
             labelPlacement="outside"
-            placeholder="CHOOSE SYSTEM"
+            placeholder="CHOOSE TEMPLATE"
             selectorIcon={<IoIosArrowDown color="var(--bg-primary-color)" />}
             classNames={{
-              base: "p-[2px] max-w-xs peer w-48 md:w-64 self-center rounded-lg border-2 dark:border-slate-400 border-black border-opacity-55 text-xs bg-inherit focus:outline-none focus:border-cyan-300",
+              base: "p-[2px] max-w-xs peer w-52 md:w-64 self-center rounded-lg border-2 dark:border-slate-400 border-black border-opacity-55 text-xs bg-inherit focus:outline-none focus:border-cyan-300",
               trigger: "h-7",
             }}
-            renderValue={(items) => {
-              return items.map((item) => (
-                <div key={item.key} className="flex items-center gap-2">
-                  <Avatar
-                    alt={item.data.name}
-                    className="flex-shrink-0"
-                    size="sm"
-                    src={`/images/coins/${item.key}.png`}
-                  />
-                  <div className="flex flex-col">
-                    <span>{item.data.balance}</span>
-                    <span className="text-default-500 text-tiny">
-                      {item.data.name}
-                    </span>
-                  </div>
-                </div>
-              ));
-            }}
           >
-            {(item) => (
-              <SelectItem key={item["name"]} textValue={item["name"]}>
-                <div className="flex gap-2 items-center">
-                  <Avatar
-                    alt={item["name"]}
-                    className="flex-shrink-0"
-                    size="sm"
-                    src={`/images/coins/${item["name"]}.png`}
-                  />
-                  <div className="flex flex-col">
-                    <span className="text-small">{item["balance"]}</span>
-                    <span className="text-tiny text-default-400">
-                      {item["name"]}
-                    </span>
-                  </div>
-                </div>
+            {templates.map((template) => (
+              <SelectItem key={template} value={template}>
+                {template}
               </SelectItem>
-            )}
+            ))}
           </Select>
         </div>
-        <div className="flex m-auto w-fit md:w-full md:gap-4 gap-2 items-center">
-          <label className="text-right text-sm md:text-base w-14 md:w-36 mt-3">
+        <div className="flex w-full gap-4 items-center">
+          <label className="text-right text-sm md:text-base w-36 mt-3">
             Amount
           </label>
           <MyInput
@@ -181,12 +116,12 @@ export default function Add(props) {
               placeholder: "$0",
             }}
           />
-          <p className="w-24 text-center pt-[3px] mt-3 h-[34px] bg-inherit border-2 dark:border-slate-400 border-black border-opacity-55 rounded-md">
+          <p className="w-24 pt-[3px] mt-3 h-[34px] bg-inherit border-2 dark:border-slate-400 border-black border-opacity-55 rounded-md">
             USD
           </p>
         </div>
-        <div className="flex m-auto w-fit md:w-full md:gap-4 gap-2 items-center">
-          <label className="text-right text-sm md:text-base w-14 md:w-36 mt-3">
+        <div className="flex w-full gap-4 items-center">
+          <label className="text-right text-sm md:text-base w-36 mt-3">
             Total
           </label>
           <MyInput
@@ -217,11 +152,9 @@ export default function Add(props) {
             ))}
           </Select>
         </div>
-        <div className="w-fit m-auto md:m-0">
-          <Button className="bg-orange text-white rounded-full mt-4 md:px-10 w-12">
-            ADD
-          </Button>
-        </div>
+        <Button className="bg-orange text-white rounded-full mt-4 md:px-10">
+          ADD
+        </Button>
       </div>
     </div>
   );
