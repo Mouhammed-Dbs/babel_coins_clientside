c8b350d3c8c0113517fef27282bb8bb99d9ebea5
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-03-12 15:12:06 +0200

    continue

--- src/pages/account/add.js
+++ src/pages/account/add.js
@@ -70,6 +70,8 @@ export default function Add(props) {
               .catch((err) => {
                 setAddressesLoading(false);
               });
+          } else {
+            setAddressesLoading(false);
           }
         }
         setPageLoading(false);
