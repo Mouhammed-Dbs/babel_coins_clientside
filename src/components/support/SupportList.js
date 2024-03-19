e5b97484a6681ef363d4855c3048a8d8675aaa1c
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-03-19 12:10:27 +0200

    fix some issues and make page responsive

--- src/components/support/SupportList.js
+++ src/components/support/SupportList.js
@@ -1,7 +1,105 @@
+import { IoCheckmarkDoneCircle, IoTimeOutline } from "react-icons/io5";
+
 export default function SupportList() {
   return (
     <div
-      className={`w-[78%] md:11/12 mt-5 rounded-md md:p-6 p-4 bg-white dark:bg-default-100 shadow-md`}
-    ></div>
+      className={`w-full mt-5 rounded-md md:p-6 py-4 px-1 bg-white dark:bg-default-100 shadow-md`}
+    >
+      <div className="mx-1 md:mx-4">
+        <div className="hidden md:flex md:gap-4 py-2 px-2 mt-3 font-bold text-gray-700 dark:text-gray-300 text-center">
+          <h3 className="w-3/12 text-sm text-start">SUBJECT</h3>
+          <h3 className="w-2/12 text-sm">DATE</h3>
+          <h3 className="w-2/12 text-sm">EDITED</h3>
+          <h3 className="w-2/12 text-sm">CNT</h3>
+          <h3 className="w-2/12 text-sm">ID</h3>
+          <h3 className="w-1/12 text-sm">STATUS</h3>
+        </div>
+        <ul className="w-full">
+          <ItemListSupport
+            subject="this is subject box it is responsive on many screens"
+            date="16 Jan 2024 23:52"
+            edited="2 Jul 2024 23:52"
+            cnt="1"
+            id="32907430"
+            status={false}
+          />
+          <ItemListSupport
+            subject="this is subject box this is subject box it is responsive on many screens"
+            date="16 Jan 2024 23:52"
+            edited="2 Jul 2024 23:52"
+            cnt="1"
+            id="32907430"
+            status={true}
+          />
+          <ItemListSupport
+            subject="this is subject box it is responsive on many screens"
+            date="16 Jan 2024 23:52"
+            edited="2 Jul 2024 23:52"
+            cnt="1"
+            id="32907430"
+            status={false}
+          />
+          <ItemListSupport
+            subject="this is subject box it is responsive on many screens"
+            date="16 Jan 2024 23:52"
+            edited="2 Jul 2024 23:52"
+            cnt="1"
+            id="32907430"
+            status={false}
+          />
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function ItemListSupport({ subject, date, edited, cnt, id, status }) {
+  return (
+    <li className="md:flex md:gap-4 border-b-1 md:border-b-2 py-3 px-2 font-bold text-left md:text-center">
+      <div className="flex md:w-3/12 w-full py-2 md:py-1">
+        <label className="block md:hidden text-sm mr-2 text-center min-w-20">
+          SUBJECT
+        </label>
+        <p className="w-full text-sm opacity-70 self-center md:text-left">
+          {subject}
+        </p>
+      </div>
+      <div className="flex md:w-2/12 w-full py-2 md:py-1">
+        <label className="block md:hidden text-sm mr-2 text-center min-w-20">
+          DATE
+        </label>
+        <p className="w-full text-sm opacity-70 self-center md:px-4">{date}</p>
+      </div>
+      <div className="flex md:w-2/12 w-full py-2 md:py-1">
+        <label className="block md:hidden text-sm mr-2 text-center min-w-20">
+          EDITED
+        </label>
+        <p className="w-full text-sm opacity-70 self-center md:px-4">
+          {edited}
+        </p>
+      </div>
+      <div className="flex md:w-2/12 w-full py-2 md:py-1">
+        <label className="block md:hidden text-sm mr-2 text-center min-w-20">
+          CNT
+        </label>
+        <p className="w-full text-sm opacity-70 self-center md:px-4">{cnt}</p>
+      </div>
+      <div className="flex md:w-2/12 w-full py-2 md:py-1">
+        <label className="block md:hidden text-sm mr-2 text-center min-w-20">
+          ID
+        </label>
+        <p className="w-full text-sm opacity-70 self-center md:px-4">{id}</p>
+      </div>
+      <div className="flex md:w-1/12 w-full overflow-hidden md:place-content-center place-content-start py-2 md:py-1">
+        <label className="block md:hidden text-sm mr-2 text-center min-w-20">
+          STATUS
+        </label>
+        {status ? (
+          <IoCheckmarkDoneCircle className="w-5 h-5 md:h-8 md:w-8 self-center  text-green-400" />
+        ) : (
+          <IoTimeOutline className="w-5 h-5 md:h-8 md:w-8 self-center text-gray-500" />
+        )}
+      </div>
+    </li>
   );
 }
--- src/pages/account/support.js
+++ src/pages/account/support.js
@@ -5,56 +5,54 @@ import { useState } from "react";
 export default function Support() {
   const [tab, setTab] = useState("list");
   return (
-    <div className="h-screen container m-auto no-scrollbar overflow-y-scroll pb-[150px]">
-      <div className="w-full md:w-[720px] lg:w-[950px] ml-4 mt-4 pb-3">
-        <div className="w-fit ml-2 md:ml-4 bg-gradient-to-r from-black dark:from-slate-300 via-gray-600 to-default-300 dark:bg-default-50 pb-[2px]">
+    <div className="h-screen container m-auto no-scrollbar overflow-y-scroll pl-5 md:pl-10 pb-[150px]">
+      <div className="w-full md:w-[720px] lg:w-[950px] md:m-auto mt-4 md:mt-4 pb-3">
+        <div className="w-fit bg-gradient-to-r from-black dark:from-slate-300 via-gray-600 to-default-300 dark:bg-default-50 pb-[2px]">
           <h1 className="w-fit text-lg md:text-2xl font-bold bg-slate-50 dark:bg-default-50">
             SUPPORT
           </h1>
         </div>
-        <p className="text-xs opacity-75 mt-1 ml-2 md:ml-4">
+        <p className="w-full text-xs opacity-75 mt-1 pr-4">
           Operator of a support service will answer you within 1-24 hours
         </p>
       </div>
-      <div className="ml-2 md:ml-4 mt-2 md:mt-5 md:m-auto w-11/12 md:w-[720px] lg:w-[950px]">
-        <div className="absolute container h-screen ml-3 no-scrollbar overflow-y-scroll pb-[150px]">
+      <div className="mt-2 md:mt-5 md:m-auto w-11/12 md:w-[720px] lg:w-[950px]">
+        <div className="container h-screen">
           {/* Tabs */}
-          <div className="w-screen overflow-x-hidden">
-            <Tabs
-              selectedKey={tab}
-              onSelectionChange={setTab}
-              aria-label="Options"
-              variant="underlined"
-              classNames={{
-                tabList: "ml-1 gap-6 relative rounded-none p-0 ",
-                cursor: "w-full bg-[var(--primary-color)]",
-                tab: "max-w-fit px-0 h-12",
-                tabContent:
-                  "group-data-[selected=true]:text-[var(--primary-color)]",
-              }}
+          <Tabs
+            selectedKey={tab}
+            onSelectionChange={setTab}
+            aria-label="Options"
+            variant="underlined"
+            classNames={{
+              tabList: "ml-1 gap-6 relative rounded-none p-0 ",
+              cursor: "w-full bg-[var(--primary-color)]",
+              tab: "max-w-fit px-0 h-12",
+              tabContent:
+                "group-data-[selected=true]:text-[var(--primary-color)]",
+            }}
+          >
+            <Tab
+              onSelect={() => setTab("newticket_tab")}
+              key="list"
+              title={
+                <div className="flex items-center space-x-2">
+                  <span>List</span>
+                </div>
+              }
             >
-              <Tab
-                onSelect={() => setTab("newticket_tab")}
-                key="list"
-                title={
-                  <div className="flex items-center space-x-2">
-                    <span>List</span>
-                  </div>
-                }
-              >
-                <SupportList />
-              </Tab>
-              <Tab
-                key="newticket_tab"
-                onSelect={() => setTab("list")}
-                title={
-                  <div className="flex items-center space-x-2">
-                    <span>New Ticket</span>
-                  </div>
-                }
-              ></Tab>
-            </Tabs>
-          </div>
+              <SupportList />
+            </Tab>
+            <Tab
+              key="newticket_tab"
+              onSelect={() => setTab("list")}
+              title={
+                <div className="flex items-center space-x-2">
+                  <span>New Ticket</span>
+                </div>
+              }
+            ></Tab>
+          </Tabs>
         </div>
       </div>
     </div>
