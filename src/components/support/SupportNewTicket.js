ae17bc6f7abaf3ded93f2fe720367ed68b0abb05
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-03-19 16:09:45 +0200

    add select and send button

--- src/components/support/SupportNewTicket.js
+++ src/components/support/SupportNewTicket.js
@@ -1,13 +1,14 @@
-import { Button } from "@nextui-org/react";
+import { Button, Select, SelectItem } from "@nextui-org/react";
 import MyInput from "../utils/MyInput";
 import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function SupportNewTicket() {
   const [numFiles, setNumFiles] = useState(1);
   const [files, setFiles] = useState(["f0"]);
   return (
     <div
-      className={`w-full mt-5 rounded-md md:p-6 py-4 px-1 bg-white dark:bg-default-100 shadow-md`}
+      className={`w-full mt-5 rounded-md md:p-6 py-4 px-3 bg-white dark:bg-default-100 shadow-md`}
     >
       <MyInput
         color="border-gray-500"
@@ -31,7 +32,7 @@ export default function SupportNewTicket() {
       />
       <div className="relative mt-3">
         <textarea
-          className={`w-96 h-44 resize-none self-center placeholder-slate-300 mt-6 rounded-lg border-2 text-xs p-2 bg-inherit scrollbar-hide border-gray-500 focus:outline-none focus:border-cyan-300`}
+          className={`w-full md:w-96 h-44 resize-none self-center placeholder-slate-300 mt-6 rounded-lg border-2 text-xs p-2 bg-inherit scrollbar-hide border-gray-500 focus:outline-none focus:border-cyan-300`}
           type="text"
           placeholder=""
         ></textarea>
@@ -56,6 +57,32 @@ export default function SupportNewTicket() {
         >
           more
         </Button>
+        <Select
+          defaultSelectedKeys={["other_que"]}
+          disallowEmptySelection={true}
+          label="Category:"
+          style={{ backgroundColor: "inherit" }}
+          size="sm"
+          labelPlacement="outside"
+          selectorIcon={<IoIosArrowDown color="var(--bg-primary-color)" />}
+          classNames={{
+            base: "p-[2px] mt-8 w-full md:w-96 self-center rounded-lg border-2 dark:border-slate-400 border-black border-opacity-55 text-xs bg-inherit focus:outline-none focus:border-cyan-300",
+            trigger: "h-7",
+          }}
+        >
+          <SelectItem key="other_que" value="other_que">
+            Other questions
+          </SelectItem>
+          <SelectItem key="aml" value="aml">
+            AML
+          </SelectItem>
+          <SelectItem key="mastercard" value="mastercard">
+            BabelCoins MasterCard
+          </SelectItem>
+        </Select>
+        <Button className="bg-orange text-white rounded-full mt-4 px-8">
+          SEND
+        </Button>
       </div>
     </div>
   );
@@ -66,8 +93,8 @@ function ItemFile({ id }) {
   return (
     <div className="block py-1">
       <label
-        for={`file-upload-` + id}
-        class="p-1 text-sm rounded-md cursor-pointer border-2 border-gray-500"
+        htmlFor={`file-upload-` + id}
+        className="p-1 text-sm rounded-md cursor-pointer border-2 border-gray-500"
       >
         Choose a file {valFile}
       </label>
