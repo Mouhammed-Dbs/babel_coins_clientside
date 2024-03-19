ad4e13355734a65d61ec28dc49f73ce3cb99e9b9
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-03-19 10:48:26 +0200

    create components for support page and fix tabs card

---
+++ src/components/support/SupportList.js
@@ -0,0 +1,7 @@
+export default function SupportList() {
+  return (
+    <div
+      className={`w-[78%] md:11/12 mt-5 rounded-md md:p-6 p-4 bg-white dark:bg-default-100 shadow-md`}
+    ></div>
+  );
+}
