361e332c1a37549b89d8edddbbf5da3017a2810f
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-03-28 17:34:16 +0200

    continue

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -38,105 +38,104 @@ export default function Contacts() {
         className="w-5/6 md:w-2/3 m-auto mt-4 p-4 px-6 text-white"
         style={{ backgroundColor: "rgb(255,255,255,0.1)" }}
       >
-        <CardHeader>
-          <h1 className="w-full text-center text-xl">CONTACT US</h1>
-        </CardHeader>
-        <CardBody className="grid md:grid-template-columns md:gap-x-4">
-          <form
-            className="w-full"
-            onSubmit={(event) => {
-              sendMessage(event);
-            }}
-          >
-            <MyInput
-              textColor="text-white"
-              className="w-full md:w-72"
-              onChange={(e) => setNameValue(e.target.value)}
-              value={nameValue}
-              item={{
-                name: "text",
-                type: "text",
-                placeholder: "John",
-                label: "Name",
-              }}
-            />
-            <MyInput
-              textColor="text-white"
-              className="mt-3 w-full md:w-72"
-              onChange={(e) => setEmailValue(e.target.value)}
-              value={emailValue}
-              item={{
-                name: "email",
-                type: "email",
-                placeholder: "you@example.com",
-                label: "Email",
-              }}
-            />
+        <form
+          onSubmit={(event) => {
+            sendMessage(event);
+          }}
+        >
+          <CardHeader>
+            <h1 className="w-full text-center text-xl">CONTACT US</h1>
+          </CardHeader>
+          <CardBody className="grid md:grid-template-columns md:gap-x-4">
+            <div className="w-full">
+              <MyInput
+                textColor="text-white"
+                className="w-full md:w-72"
+                onChange={(e) => setNameValue(e.target.value)}
+                value={nameValue}
+                item={{
+                  name: "text",
+                  type: "text",
+                  placeholder: "John",
+                  label: "Name",
+                }}
+              />
+              <MyInput
+                textColor="text-white"
+                className="mt-3 w-full md:w-72"
+                onChange={(e) => setEmailValue(e.target.value)}
+                value={emailValue}
+                item={{
+                  name: "email",
+                  type: "email",
+                  placeholder: "you@example.com",
+                  label: "Email",
+                }}
+              />
 
-            <div className="relative mt-3">
-              <textarea
-                value={messValue}
-                onChange={(e) => {
-                  setMessValue(e.target.value);
-                }}
-                className={`peer/email w-full md:w-72 h-24 resize-none self-center text-white placeholder-slate-300 mt-6 rounded-lg border-2 text-xs p-2 bg-inherit focus:outline-none focus:border-cyan-300 scrollbar-hide ${
-                  messValue
-                    ? "border-cyan-300"
-                    : "border-white border-opacity-35"
-                }`}
-                type="text"
-                placeholder="Enter your message"
-              ></textarea>
-              <label
-                className={`absolute -top-0 -left-0 text-sm ml-1 mb-1 peer-focus/email:text-cyan-300 ${
-                  messValue ? "text-cyan-300" : "text-white text-opacity-65"
-                }`}
-              >
-                Your message
-              </label>
+              <div className="relative mt-3">
+                <textarea
+                  value={messValue}
+                  onChange={(e) => {
+                    setMessValue(e.target.value);
+                  }}
+                  className={`peer/email w-full md:w-72 h-24 resize-none self-center text-white placeholder-slate-300 mt-6 rounded-lg border-2 text-xs p-2 bg-inherit focus:outline-none focus:border-cyan-300 scrollbar-hide ${
+                    messValue
+                      ? "border-cyan-300"
+                      : "border-white border-opacity-35"
+                  }`}
+                  type="text"
+                  placeholder="Enter your message"
+                ></textarea>
+                <label
+                  className={`absolute -top-0 -left-0 text-sm ml-1 mb-1 peer-focus/email:text-cyan-300 ${
+                    messValue ? "text-cyan-300" : "text-white text-opacity-65"
+                  }`}
+                >
+                  Your message
+                </label>
+              </div>
+              <div className="flex mt-3 ml-1">
+                <input
+                  onChange={() => {
+                    setAgreeValue(!agreeValue);
+                  }}
+                  checked={agreeValue}
+                  value={agreeValue}
+                  type="checkbox"
+                  className="peer accent-white"
+                ></input>
+                <label className="text-xs peer-checked:text-cyan-300 text-white text-opacity-65 ml-2">
+                  I agree to the processing of the personal data provided
+                </label>
+              </div>
+              <ErrorMessage show={errorForm.error} message={errorForm.msg} />
             </div>
-            <div className="flex mt-3 ml-1">
-              <input
-                onChange={() => {
-                  setAgreeValue(!agreeValue);
-                }}
-                checked={agreeValue}
-                value={agreeValue}
-                type="checkbox"
-                className="peer accent-white"
-              ></input>
-              <label className="text-xs peer-checked:text-cyan-300 text-white text-opacity-65 ml-2">
-                I agree to the processing of the personal data provided
-              </label>
+            <div className="hidden md:block pl-1">
+              <p>
+                hi you cckals f ef eg rgergre ger g rewgegyjhy uk iol oil 0p; 0
+                ; ik7 uy5hy5hg54 tg45 g3 g43ef324f 4ghy j 8k 798k 8 u7j 6 j6
+              </p>
+              <div className="w-full flex justify-center mt-8">
+                <Image
+                  src={"/images/logo.svg"}
+                  alt=""
+                  width={40}
+                  height={40}
+                ></Image>
+              </div>
             </div>
-            <ErrorMessage show={errorForm.error} message={errorForm.msg} />
-            <div className="mt-4">
-              <Button
-                type="submit"
-                isDisabled={
-                  !(agreeValue && nameValue && emailValue && messValue)
-                }
-                className="bg-orange rounded-full block m-auto md:m-0 text-white h-8 w-36 hover:bg-white hover:text-orange"
-              >
-                SEND
-              </Button>
-            </div>
-          </form>
-          <div className="hidden md:block pl-1">
-            <p>
-              hi you cckals f ef eg rgergre ger g rewgegyjhy uk iol oil 0p; 0 ;
-              ik7 uy5hy5hg54 tg45 g3 g43ef324f 4ghy j 8k 798k 8 u7j 6 j6
-            </p>
-            <div className="w-full flex justify-center mt-8">
-              <Image
-                src={"/images/logo.svg"}
-                alt=""
-                width={40}
-                height={40}
-              ></Image>
-            </div>
-          </div>
-        </CardBody>
+          </CardBody>
+          <CardFooter>
+            <Button
+              type="submit"
+              isDisabled={!(agreeValue && nameValue && emailValue && messValue)}
+              className="bg-orange rounded-full block m-auto text-white h-8 w-36 hover:bg-white hover:text-orange"
+            >
+              SEND
+            </Button>
+          </CardFooter>
+        </form>
       </Card>
     </div>
   );
