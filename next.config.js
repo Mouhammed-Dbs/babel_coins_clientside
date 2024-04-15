9a0a8f08f24845462163dea6b0dcc5a44afbf614
Mouhammed-Dbs <mouhammeddbs@gmail.com>
2024-04-15 12:05:52 +0200

    remove manual adding PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 const nextConfig = {
   reactStrictMode: false,
   sassOptions: {
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,17 +7,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <div id="installContainer" style={{ display: "none !important" }}>
-          <button id="butInstall" type="button">
-            Install
-          </button>
-        </div>
-        <script src="/app/script.js" defer></script>
-        <div
-          className="glitchButton"
-          style={{ position: "fixed", top: "20px", right: "20px" }}
-        ></div>
-        <script src="/app/button.js" defer></script>
       </body>
     </Html>
   );
